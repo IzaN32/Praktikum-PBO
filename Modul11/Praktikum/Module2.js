03485e788734c4ad0e3b2d8045f531fc8b82e24d
Rize Hersch <34876769+IzaN32@users.noreply.github.com>
2021-01-04 23:18:46 +0700

    Add files via upload

---
+++ Modul11/Praktikum/Module2.js
@@ -0,0 +1,31 @@
+var person = (function (){
+    var age = 25
+
+    function setAge(newAge){
+        age = newAge
+    }
+
+    function getAge(){
+        return age
+    }
+
+    function growOlder(){
+        age++
+    }
+
+    return {
+        name : 'Paimon',
+        setAge : setAge,
+        getAge : getAge,
+        growOlder : growOlder
+    }
+}())
+
+console.log("Nama person \t: ", person.name);
+console.log("Nama person \t: ", person.getAge());
+
+person.setAge(100)
+console.log('Umur person yang diganti\t: ', person.getAge());
+
+person.growOlder()
+console.log('Umur person yang ditambah\t: ', person.getAge());
